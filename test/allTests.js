dce74bd04d5cce825bd8aba32e60d79617e1232d
mhagnumdw <dwouglas@gmail.com>
2018-04-26 14:39:57 -0300

    Passa a testar tambem a versao minificada

--- test/allTests.js
+++ test/allTests.js
@@ -2,245 +2,227 @@ const expect = require('chai').expect;
 
 const sinon = require('sinon');
 
-const CustomWebSocket = require('../custom-websocket');
-
 const WebSocket = require('ws');
 global.WebSocket = WebSocket;
 
 const WebSocketServer = require('ws').Server;
 
+const anyMessageText = 'hello';
+
 const PORT = 50123;
 const URL = `ws://localhost:${PORT}`;
 
-describe('custom-websocket', function () {
+var wss = null;
+var cws = null;
 
-    it('send text, receive and close with default options', function (done) {
+afterEach(function () {
+    close(wss, cws);
+});
 
-        afterEach(function () {
-            close(wss, cws);
-        });
+var instances = [
+    {cws: require('../custom-websocket.js'), description: 'normal version'},
+    {cws: require('../custom-websocket.min.js'), description: 'minified version'}
+];
 
-        const anyMessageText = 'hello';
-        const wss = new WebSocketServer({port: PORT});
+instances.forEach(function (instance) {
 
-        wss.on('connection', function connection(ws, req) {
-            ws.on('message', function incoming(message) {
-                ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+    describe('custom-websocket: ' + instance.description, function () {
+
+        const CustomWebSocket = instance.cws;
+
+        it('send text, receive and close with default options', function (done) {
+
+            wss = new WebSocketServer({port: PORT});
+
+            wss.on('connection', function connection(ws, req) {
+                ws.on('message', function incoming(message) {
+                    ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+                });
             });
-        });
 
-        const cws = new CustomWebSocket(URL);
+            cws = new CustomWebSocket(URL);
 
-        expect(cws.readyState).to.be.equal(WebSocket.CONNECTING);
+            expect(cws.readyState).to.be.equal(WebSocket.CONNECTING);
 
-        cws.addEventListener('open', function(event) {
-            expect(cws.readyState).to.be.equal(WebSocket.OPEN);
-            cws.send(anyMessageText);
-        });
-
-        cws.addEventListener('message', function(event) {
-            expect(event.data).to.match(new RegExp("^SERVER_SAYS:(" + anyMessageText + "|PING)$"));
-            cws.close();
-        });
-
-        cws.addEventListener('close', function(event) {
-            expect(cws.readyState).to.be.equal(WebSocket.CLOSED);
-            done();
-        });
-
-    });
-
-    it('ping (heartbeat)', function (done) {
-
-        afterEach(function () {
-            close(wss, cws);
-        });
-
-        const wss = new WebSocketServer({port: PORT});
-
-        wss.on('connection', function connection(ws, req) {
-            ws.on('message', function incoming(message) {
-                ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+            cws.addEventListener('open', function(event) {
+                expect(cws.readyState).to.be.equal(WebSocket.OPEN);
+                cws.send(anyMessageText);
             });
-        });
 
-        const cws = new CustomWebSocket(URL, null, {heartbeatInterval: 3000});
+            cws.addEventListener('message', function(event) {
+                expect(event.data).to.match(new RegExp("^SERVER_SAYS:(" + anyMessageText + "|PING)$"));
+                cws.close();
+            });
 
-        cws.addEventListener('message', function(event) {
-            expect(event.data).to.match(new RegExp("^SERVER_SAYS:PING$"));
-            done();
-        });
-
-    }).timeout(6000); // timeout 2x heartbeatInterval to ensure
-
-    it('reconnect - with default options', function (done) {
-
-        afterEach(function () {
-            close(wss, cws);
-        });
-
-        var wss = new WebSocketServer({port: PORT});
-        const cws = new CustomWebSocket(URL);
-
-        var firstConnection = true;
-        cws.addEventListener('open', function(event) {
-            if (firstConnection) {
-                firstConnection = false;
-                wss.close();
-            } else { // if second connection, then it was reconnected
+            cws.addEventListener('close', function(event) {
+                expect(cws.readyState).to.be.equal(WebSocket.CLOSED);
                 done();
-            }
+            });
+
         });
 
-        var attempt = 0;
-        cws.addEventListener('error', function(event) {
-            if (++attempt == 3) { // to simulate the server back
-                wss = new WebSocketServer({port: PORT});
-            }
-        });
+        it('ping (heartbeat)', function (done) {
 
-    }).timeout(10000);
+            wss = new WebSocketServer({port: PORT});
 
-    it('reconnect - with shouldReconnect as number', function (done) {
+            wss.on('connection', function connection(ws, req) {
+                ws.on('message', function incoming(message) {
+                    ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+                });
+            });
 
-        afterEach(function () {
-            close(wss, cws);
-        });
+            cws = new CustomWebSocket(URL, null, {heartbeatInterval: 3000});
 
-        var wss = new WebSocketServer({port: PORT});
-        var previousTime = null;
-        const cws = new CustomWebSocket(URL, null, {shouldReconnect: 1500});
-        var firstConnection = true;
+            cws.addEventListener('message', function(event) {
+                expect(event.data).to.match(new RegExp("^SERVER_SAYS:PING$"));
+                done();
+            });
 
-        cws.addEventListener('open', function(event) {
-            if (firstConnection) {
-                firstConnection = false;
-                wss.close();
+        }).timeout(6000); // timeout 2x heartbeatInterval to ensure
+
+        it('reconnect - with default options', function (done) {
+
+            wss = new WebSocketServer({port: PORT});
+            cws = new CustomWebSocket(URL);
+
+            var firstConnection = true;
+            cws.addEventListener('open', function(event) {
+                if (firstConnection) {
+                    firstConnection = false;
+                    wss.close();
+                } else { // if second connection, then it was reconnected
+                    done();
+                }
+            });
+
+            var attempt = 0;
+            cws.addEventListener('error', function(event) {
+                if (++attempt == 3) { // to simulate the server back
+                    wss = new WebSocketServer({port: PORT});
+                }
+            });
+
+        }).timeout(10000);
+
+        it('reconnect - with shouldReconnect as number', function (done) {
+
+            wss = new WebSocketServer({port: PORT});
+            cws = new CustomWebSocket(URL, null, {shouldReconnect: 1500});
+            var previousTime = null;
+            var firstConnection = true;
+
+            cws.addEventListener('open', function(event) {
+                if (firstConnection) {
+                    firstConnection = false;
+                    wss.close();
+                    previousTime = Date.now();
+                } else { // if second connection, then it was reconnected
+                    done();
+                }
+            });
+
+            var attempt = 0;
+            cws.addEventListener('error', function(event) {
+                var waited = Date.now() - previousTime;
+                expect(waited).to.be.closeTo(1400, 1600); // because shouldReconnect is 1500 ms
                 previousTime = Date.now();
-            } else { // if second connection, then it was reconnected
+                if (++attempt == 3) { // to simulate the server coming back
+                    wss = new WebSocketServer({port: PORT});
+                }
+            });
+
+        }).timeout(10000);
+
+        it('automaticOpen = false, check if connected', function (done) {
+
+            wss = new WebSocketServer({port: PORT});
+            cws = new CustomWebSocket(URL, null, {automaticOpen: false});
+
+            cws.addEventListener('open', function(event) {
+                expect.fail("passed here", "should not passed here");
+            });
+
+            // TODO: assert readyState = -1 ?
+            // For this, readyState in custom-websocket.js instead begin with
+            // "this.readyState = WebSocket.CONNECTING;" should be start with -1
+            // and inside "this.open = function () {" one of the first things
+            // should be set "this.readyState = WebSocket.CONNECTING;"
+
+            setTimeout(function() {
+                var badFn = function () { cws.send(anyMessageText); };
+                expect(badFn).to.throw();
                 done();
-            }
-        });
+            } , 3000);
 
-        var attempt = 0;
-        cws.addEventListener('error', function(event) {
-            var waited = Date.now() - previousTime;
-            expect(waited).to.be.closeTo(1400, 1600); // because shouldReconnect is 1500 ms
-            previousTime = Date.now();
-            if (++attempt == 3) { // para simular o servidor voltando
-                wss = new WebSocketServer({port: PORT});
-            }
-        });
+        }).timeout(6000);
 
-    }).timeout(10000);
+        it('automaticOpen = false, manual connect', function (done) {
 
-    it('automaticOpen = false, check if connected', function (done) {
+            wss = new WebSocketServer({port: PORT});
+            cws = new CustomWebSocket(URL, null, {automaticOpen: false});
 
-        afterEach(function () {
-            close(wss, cws);
-        });
-
-        const anyMessageText = 'hello';
-        const wss = new WebSocketServer({port: PORT});
-        const cws = new CustomWebSocket(URL, null, {automaticOpen: false});
-
-        cws.addEventListener('open', function(event) {
-            expect.fail("passed here", "should not passed here");
-        });
-
-        // TODO: assert readyState = -1 ?
-        // For this, readyState in custom-websocket.js instead begin with
-        // "this.readyState = WebSocket.CONNECTING;" should be start with -1
-        // and inside "this.open = function () {" one of the first things
-        // should be set "this.readyState = WebSocket.CONNECTING;"
-
-        setTimeout(function() {
-            var badFn = function () { cws.send(anyMessageText); };
-            expect(badFn).to.throw();
-            done();
-        } , 3000);
-
-    }).timeout(6000);
-
-    it('automaticOpen = false, manual connect', function (done) {
-
-        afterEach(function () {
-            close(wss, cws);
-        });
-
-        const anyMessageText = 'hello';
-        const wss = new WebSocketServer({port: PORT});
-        const cws = new CustomWebSocket(URL, null, {automaticOpen: false});
-
-        cws.addEventListener('open', function(event) {
-            var goodFn = function () { cws.send(anyMessageText); };
-            expect(goodFn).to.not.throw();
-        });
-
-        cws.open();
-
-        done();
-
-    });
-
-    it('no ping (heartbeat = false)', function (done) {
-
-        afterEach(function () {
-            close(wss, cws);
-        });
-
-        const wss = new WebSocketServer({port: PORT});
-
-        wss.on('connection', function connection(ws, req) {
-            ws.on('message', function incoming(message) {
-                ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+            cws.addEventListener('open', function(event) {
+                var goodFn = function () { cws.send(anyMessageText); };
+                expect(goodFn).to.not.throw();
             });
-        });
 
-        const cws = new CustomWebSocket(URL, null, {heartbeat: false, heartbeatInterval: 1000});
+            cws.open();
 
-        var timeout = setTimeout(function() {
             done();
-        } , 3000); // time sufficient to client send a PING, but in this test this should not happen
 
-        cws.addEventListener('message', function(event) {
-            clearTimeout(timeout); // not necessary wait timeout because this test has already failed here
-            done();
-            expect.fail("received this message: " + event.data, "should not have received any message");
         });
 
-    }).timeout(4000); // timeout 4x heartbeatInterval to ensure
+        it('no ping (heartbeat = false)', function (done) {
 
-    it('debug = true', function (done) {
+            wss = new WebSocketServer({port: PORT});
 
-        afterEach(function () {
-            close(wss, cws);
-            expect( console.debug.callCount ).to.equal(6); // console.debug must be called 6 times by API
-            spy.restore();
-        });
-
-        let spy = sinon.spy(console, 'debug');
-        const anyMessageText = 'hello';
-        const wss = new WebSocketServer({port: PORT});
-
-        wss.on('connection', function connection(ws, req) {
-            ws.on('message', function incoming(message) {
-                ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+            wss.on('connection', function connection(ws, req) {
+                ws.on('message', function incoming(message) {
+                    ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+                });
             });
-        });
 
-        const cws = new CustomWebSocket(URL, null, {debug: true});
+            cws = new CustomWebSocket(URL, null, {heartbeat: false, heartbeatInterval: 1000});
 
-        cws.addEventListener('open', function(event) {
-            cws.send(anyMessageText);
-        });
+            var timeout = setTimeout(function() {
+                done();
+            } , 3000); // time sufficient to client send a PING, but in this test this should not happen
 
-        cws.addEventListener('message', function(event) {
-            cws.close();
-        });
+            cws.addEventListener('message', function(event) {
+                clearTimeout(timeout); // not necessary wait timeout because this test has already failed here
+                done();
+                expect.fail("received this message: " + event.data, "should not have received any message");
+            });
 
-        cws.addEventListener('close', function(event) {
-            done();
+        }).timeout(4000); // timeout 4x heartbeatInterval to ensure
+
+        it('debug = true', function (done) {
+
+            let spy = sinon.spy(console, 'debug');
+            wss = new WebSocketServer({port: PORT});
+
+            wss.on('connection', function connection(ws, req) {
+                ws.on('message', function incoming(message) {
+                    ws.send("SERVER_SAYS:" + message); // server send back the message it received with prefix "SERVER_SAYS:"
+                });
+            });
+
+            cws = new CustomWebSocket(URL, null, {debug: true});
+
+            cws.addEventListener('open', function(event) {
+                cws.send(anyMessageText);
+            });
+
+            cws.addEventListener('message', function(event) {
+                cws.close();
+            });
+
+            cws.addEventListener('close', function(event) {
+                expect( console.debug.callCount ).to.equal(6); // console.debug must be called 6 times by API
+                spy.restore();
+                done();
+            });
+
         });
 
     });
